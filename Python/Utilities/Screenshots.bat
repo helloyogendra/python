9b4e3afdc230be5c7ca2653ff9a03a28eb1d7f72
Yogendra Singh <helloyogendra@gmail.com>
2024-10-28 19:10:05 +0530

    28-oct-2024-screenshots

--- Python/Utilities/Screenshots.bat
+++ Python/Utilities/Screenshots.bat
@@ -1,11 +1,11 @@
 echo The script is waiting to run...
 timeout 5
 
-cd C:\Users\YSingh\test\env_1\Scripts 
+cd C:\Users\hello\test\env_1\Scripts 
 
 call activate
 
-CD C:\Users\YSingh\git\python\Python\Utilities
+CD C:\Users\hello\git\Code\Python\Utilities
 
 start /min pythonw Screenshots.py
 exit
