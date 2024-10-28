193d61769f175e45db6f51a7ce3bdd8e786bdc83
Yogendra Singh <helloyogendra@gmail.com>
2024-10-29 05:17:48 +0530

    29-Oct-24-asynchronous

--- Python/Asynchronous/Multi_Threading_2.py
+++ Python/Asynchronous/Multi_Threading_2.py
@@ -1,28 +1,6 @@
-import threading
-import time
-
-# A simple function that simulates a time-consuming task
-def thread_task(name, delay):
-    print(f"Thread {name}: starting")
-    time.sleep(delay)
-    print(f"Thread {name}: finishing")
-
-# Create two threads
-thread1 = threading.Thread(target=thread_task, args=("One", 2))
-thread2 = threading.Thread(target=thread_task, args=("Two", 2))
-
-# Start the threads
-thread1.start()
-thread2.start()
-
-# Wait for both threads to complete
-thread1.join()
-thread2.join()
-
-print("Both threads have finished")
-
-
 # collecting result from thread example
+import threading
+
 print("\nExample : Collect result from a thread\n")
 
 GREEN = "\033[0;32m"
@@ -45,7 +23,7 @@ def get_square(name, number):
 
 # Create and start threads
 threads = []
-for i in range(1, 9, 2):
+for i in range(1, 5):
     thread = threading.Thread(target=get_square, args=(f"Thread-{i}", i))
     threads.append(thread)
     thread.start()
@@ -59,4 +37,3 @@ print("Collected results:")
 for result in results:
     print(f'{YELLOW} Fetching Result- {RESET}')
     print(f'{YELLOW} {result} {RESET}')
-
