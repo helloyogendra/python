c4c03c9ea2b3e554ef96662e7665f47fa5be5f03
Yogendra Singh <helloyogendra@gmail.com>
2024-10-29 10:21:54 +0530

    29-10-24-singleton changes

--- Python/Design_Patterns/singleton.py
+++ Python/Design_Patterns/singleton.py
@@ -1,18 +1,35 @@
-class Singleton:
+# 1.
+
+class SingletonExample1:
     _instance = None
 
     def __new__(cls):
         if cls._instance is None:
-            cls._instance = super(Singleton, cls).__new__(cls)
+            cls._instance = super(SingletonExample1, cls).__new__(cls)
         return cls._instance
 
 # Usage
-s1 = Singleton()
-s2 = Singleton()
+s1 = SingletonExample1()
+s2 = SingletonExample1()
 
 print(s1 == s2)  # Output: True, both are the same instance
+print(s1 is s1)
+
+# 2.
+
+class SingletonExample2:
+  s = None
+  def __new__(cls):
+    if cls.s is None:
+      cls.s = super().__new__(cls)
+    return cls.s
 
 
+obj1 = SingletonExample2()
+obj2 = SingletonExample2()
+
+print(obj1==obj2)
+print(obj1 is obj2)
 
 
 #
@@ -51,7 +68,7 @@ import json
 class ConfigReader:
     _instance = None
 
-    def __new__(cls, config_file='config.json'):
+    def __new__(cls, config_file='Python\\Design_Patterns\\config.json'):
         if cls._instance is None:
             cls._instance = super(ConfigReader, cls).__new__(cls)
             with open(config_file, 'r') as f:
@@ -64,3 +81,4 @@ class ConfigReader:
 # Usage
 config_reader = ConfigReader()
 db_host = config_reader.get("database_host")
+print("db_host from json config is -> ", db_host)
